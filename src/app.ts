b04845af636f5d7ab1225eb652cf95a3ed004133
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-26 09:21:27 -0600

    Init express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello typescript')
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
