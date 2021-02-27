2373237c53ab3a10f002cdadbb351294b00c6cfe
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-27 16:11:18 -0600

    Add unit tests

--- test/controllers/auth.ts
+++ test/controllers/auth.ts
@@ -1,4 +1,7 @@
 import { expect } from "chai";
+import { NextFunction, Request, Response } from "express";
+import sinon from "sinon";
+import { login } from "../../src/controllers/auth";
 
 describe("login", () => {
   describe("If email and password are submitted...", () => {
@@ -11,10 +14,30 @@ describe("login", () => {
     });
   });
   describe("If email is not submitted...", () => {
-    it("should throw an error.");
+    it("should throw an error.", () => {
+      const req = {
+        body: {
+          password: "fakepassword",
+        },
+      } as Request;
+      const res = {} as Response;
+      const next = sinon.spy();
+      login(req, res, next as NextFunction);
+      expect(next.calledOnce).to.be.true;
+    });
   });
   describe("If password is not submitted...", () => {
-    it("should throw an error.");
+    it("should throw an error.", () => {
+      const req = {
+        body: {
+          email: "test@email.com",
+        },
+      } as Request;
+      const res = {} as Response;
+      const next = sinon.spy();
+      login(req, res, next as NextFunction);
+      expect(next.calledOnce).to.be.true;
+    });
   });
 });
 
