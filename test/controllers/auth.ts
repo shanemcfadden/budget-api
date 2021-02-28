4a359b91d4294a337b8a6c16ce399eb62a473504
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-28 10:56:34 -0600

    Init signup() tests

--- test/controllers/auth.ts
+++ test/controllers/auth.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import sinon, { SinonSpy, SinonStub } from "sinon";
 import bcrypt from "bcrypt";
-import { login } from "../../src/controllers/auth";
+import { login, signup } from "../../src/controllers/auth";
 import User from "../../src/models/user";
 
 const { JWT_SECRET } = process.env;
@@ -22,6 +22,9 @@ const fakeUser = {
   firstName: "Jane",
   lastName: "Doe",
 };
+
+const mockJWT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
 describe("login", async () => {
   let req: Request;
@@ -54,8 +57,6 @@ describe("login", async () => {
 
   describe("If email and password are correct...", async () => {
     const LOGIN_SUCCESS_MESSAGE = "Login successful";
-    const mockJWT =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
     let jwtSignStub: SinonStub;
 
     beforeEach(async () => {
@@ -132,11 +133,75 @@ describe("login", async () => {
 });
 
 describe("signup", async () => {
+  let req: Request;
+  let res: MockResponse;
+  let next: SinonSpy;
+
+  beforeEach(async () => {
+    req = {
+      body: {
+        email: "fake@email.com",
+        password: "fakepassword",
+      },
+    } as Request;
+    res = {
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (object) {
+        this.body = object;
+        return;
+      },
+    };
+    next = sinon.spy();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
   describe("if email is not a duplicate...", async () => {
-    it("should respond with a success");
-    it("should return a JWT in the response");
+    const SIGNUP_SUCCESS_MESSAGE = "Sign up successful";
+    let jwtSignStub: SinonStub;
+
+    beforeEach(() => {
+      sinon.stub(User, "findByEmail").returns(null);
+      sinon.stub(User, "create").resolves({ _id: fakeUser._id });
+      // jwt.sign() Types default to the asynchronous version, which returns void
+      // Haven't found a way to get the stub to reference the synchronous sign method
+      jwtSignStub = sinon
+        .stub(jwt, "sign")
+        .returns((mockJWT as unknown) as void);
+    });
+    it("should set response status to 201", async () => {
+      await login(req, res as Response, next as NextFunction);
+      expect(res.statusCode).to.equal(201);
+    });
+    it("should send success message in res body", async () => {
+      await login(req, res as Response, next as NextFunction);
+      expect(res.body).to.have.property("message");
+      expect(res.body?.message).to.equal(SIGNUP_SUCCESS_MESSAGE);
+    });
+    it("should set a JWT with the user id that expires in 1 hour", async () => {
+      await login(req, res as Response, next as NextFunction);
+      expect(
+        jwtSignStub.calledWith({ userId: fakeUser._id }, JWT_SECRET, {
+          expiresIn: "1h",
+        })
+      ).to.be.true;
+    });
+    it("should return a JWT in the response", async () => {
+      await login(req, res as Response, next as NextFunction);
+      expect(res.body).to.have.property("token");
+      expect(res.body?.token).to.equal(mockJWT);
+    });
   });
   describe("if email is a duplicate...", async () => {
-    it("should throw an error.");
+    it("should throw an error.", async () => {
+      sinon.stub(User, "findByEmail").returns(fakeUser);
+      await signup(req, res as Response, next as NextFunction);
+      expect(next.calledOnce).to.be.true;
+    });
   });
 });
