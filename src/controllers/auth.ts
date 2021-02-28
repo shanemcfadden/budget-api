fbfe447e62aec2bde389bba051139c29dc8e2b23
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-28 10:37:14 -0600

    Pass login unit tests

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,35 @@
+import bcrypt, { hash } from "bcrypt";
 import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
 
-export const login: RequestHandler = (req, res, next) => {
-  res.status(200).json({ message: "Login successful" });
+const { JWT_SECRET } = process.env;
+
+export const login: RequestHandler = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = User.findByEmail(email);
+
+  if (!user) {
+    const error = new Error("Incorrect email or password");
+    next(error);
+    return;
+  }
+
+  const hashedPassword = user.password;
+
+  const passwordMatches = await bcrypt.compare(password, hashedPassword);
+
+  if (!passwordMatches) {
+    const error = new Error("Incorrect email or password");
+    next(error);
+    console.log("password error");
+    return;
+  }
+
+  const userId = user._id;
+  const token = jwt.sign({ userId }, JWT_SECRET!, { expiresIn: "1h" });
+  res.status(200).json({ message: "Login successful", token });
 };
 
 export const signup: RequestHandler = (req, res) => {
