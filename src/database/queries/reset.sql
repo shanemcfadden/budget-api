6972c34ff8e020b6b1172ec39596516ed2a04e52
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-01 10:21:30 -0600

    Hash password for mock user

--- src/database/queries/reset.sql
+++ src/database/queries/reset.sql
@@ -10,7 +10,7 @@ CREATE TABLE users(
 
 INSERT INTO users (id, email, pw, first_name, last_name)
     VALUES 
-        ('1', 'test@test.com', 'asdfreqwreij', 'Jane', 'Doe'),
+        ('1', 'test@test.com', '$2b$12$RKlftCLGSo0VkcpG0yqJfezbnHAbgsR8kXfsXQb2UrIws9A1MU1wq', 'Jane', 'Doe'), 
         ('2', 'test2@test.com', 'password', 'John', 'Smith'),
         ('3', 'test3@test.com', 'ThisisHashed', 'Franz', 'Liszt'),
         ('4', 'test4@test.com', 'moreHash', 'Amy', 'Beach')
