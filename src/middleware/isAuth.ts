a291652b0e76e06665deba8ac2a44e93ddf870ab
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-02 10:29:25 -0600

    Pass unit tests

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,5 +1,33 @@
-import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { Response, NextFunction } from "express";
+import { ExtendedRequest } from "../types";
 
-const isAuth = (req: Request, res: Response, next: NextFunction) => {};
+const { JWT_SECRET } = process.env;
+
+interface JWTPayload {
+  userId?: string;
+}
+
+const isAuth = (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    next();
+    return;
+  }
+  const token = authHeader.split(" ")[1];
+
+  let payload: JWTPayload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET!) as JWTPayload;
+  } catch (err) {
+    next();
+    return;
+  }
+  if (payload.userId) {
+    req.isAuth = true;
+    req.userId = payload.userId;
+  }
+  next();
+};
 
 export default isAuth;
