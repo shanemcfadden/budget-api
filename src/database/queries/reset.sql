c5c0dcf95fd0a4f331ce9c9cbbaa45caa2ca8951
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-02 11:31:44 -0600

    Require every column in users table

--- src/database/queries/reset.sql
+++ src/database/queries/reset.sql
@@ -1,11 +1,11 @@
 DROP TABLE users;
 
 CREATE TABLE users(
-    id VARCHAR(36) PRIMARY KEY,
-    email VARCHAR(100) UNIQUE,
-    pw VARCHAR(100),
-    first_name VARCHAR(100),
-    last_name VARCHAR(100)
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    pw VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL
 );
 
 INSERT INTO users (id, email, pw, first_name, last_name)
