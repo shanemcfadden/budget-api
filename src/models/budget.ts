dd0b3ceec57ef2e28198a9de1a0124f7428373f4
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-04 17:13:32 -0600

    Pass unit tests

--- src/models/budget.ts
+++ src/models/budget.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from "mysql2";
+import { OkPacket, RowDataPacket } from "mysql2";
 import { queryDb } from "../database/Database";
 import { findById } from "../util/models";
 
@@ -11,7 +11,17 @@ interface BudgetData extends NewBudgetData {
 }
 
 class Budget {
-  static async create(newBudgetData: NewBudgetData) {}
+  static async create(newBudgetData: NewBudgetData) {
+    const { title, description } = newBudgetData;
+    const results = (await queryDb("budgets/create.sql", [
+      title,
+      description,
+    ])) as OkPacket;
+
+    return {
+      _id: results.insertId,
+    };
+  }
 
   static async findById(budgetId: number) {
     return findById(budgetId, "budget");
@@ -24,9 +34,34 @@ class Budget {
     return budgets;
   }
 
-  static async update(budgetData: BudgetData) {}
+  static async update(budgetData: BudgetData) {
+    const { id, title, description } = budgetData;
+    const results = (await queryDb("budgets/update.sql", [
+      title,
+      description,
+      id,
+    ])) as OkPacket;
+    if (results.affectedRows === 1) {
+      return true;
+    } else if (!results.affectedRows) {
+      throw new Error("Budget does not exist");
+    }
+    throw new Error(
+      "Multiple rows updated due to faulty query. Fix budgets/update.sql"
+    );
+  }
 
-  static async removeById(id: number) {}
+  static async removeById(id: number) {
+    const results = (await queryDb("budgets/removeById.sql", [id])) as OkPacket;
+    if (results.affectedRows === 1) {
+      return true;
+    } else if (!results.affectedRows) {
+      throw new Error("Budget does not exist");
+    }
+    throw new Error(
+      "Multiple rows deleted due to faulty query. Fix budgets/removeById.sql"
+    );
+  }
 }
 
 export default Budget;
