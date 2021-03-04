fdaf982592341dcbf76e0fb310390286b6fc7adb
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-04 17:05:10 -0600

    Add unit tests for create(), update(), removeById()

--- test/models/budget.ts
+++ test/models/budget.ts
@@ -1,21 +1,49 @@
 import { expect } from "chai";
-import { RowDataPacket } from "mysql2";
+import { OkPacket, RowDataPacket } from "mysql2";
 import sinon, { SinonStub } from "sinon";
 import * as Database from "../../src/database/Database";
 import Budget from "../../src/models/budget";
 
 describe("Budget model", () => {
+  const newBudgetData = {
+    title: "The Addams Family budget",
+    description: "It's a bit scary...",
+  };
+  const mockBudgetData = {
+    ...newBudgetData,
+    id: 2,
+  };
+  const { id, title, description } = mockBudgetData;
+  const noAffectedRows = {
+    affectedRows: 0,
+  } as OkPacket;
+  const oneAffectedRow = {
+    affectedRows: 1,
+  } as OkPacket;
+  const twoAffectedRows = {
+    affectedRows: 2,
+  } as OkPacket;
   let queryDbStub: SinonStub;
   afterEach(() => {
     sinon.restore();
   });
+  describe("create()", () => {
+    beforeEach(() => {
+      queryDbStub = sinon.stub(Database, "queryDb").resolves({
+        insertId: 2,
+      } as OkPacket);
+    });
+    it("Should query the database", async () => {
+      await Budget.create(newBudgetData);
+      expect(queryDbStub.calledOnce).to.be.true;
+    });
+    it("Should return the budget id", async () => {
+      const results = await Budget.create(newBudgetData);
+      expect(results).to.deep.equal({ _id: 2 });
+    });
+  });
   describe("findById()", () => {
     const budgetId = 30;
-    const mockBudgetData = {
-      id: 2,
-      title: "The Addams Family budget",
-      description: "It's a bit scary...",
-    };
     it("should query the database", async () => {
       queryDbStub = sinon
         .stub(Database, "queryDb")
@@ -45,11 +73,6 @@ describe("Budget model", () => {
   });
   describe("findAllByUserId()", () => {
     const userId = "asdfwerwqiohon";
-    const mockBudgetData = {
-      id: 2,
-      title: "The Addams Family budget",
-      description: "It's a bit scary...",
-    };
     it("should query the database", async () => {
       queryDbStub = sinon
         .stub(Database, "queryDb")
@@ -75,6 +98,124 @@ describe("Budget model", () => {
           .resolves([] as RowDataPacket[]);
         const result = await Budget.findAllByUserId(userId);
         expect(result).to.deep.equal([]);
+      });
+    });
+  });
+  describe("update()", () => {
+    describe("If budget exists", () => {
+      beforeEach(() => {
+        queryDbStub = sinon.stub(Database, "queryDb").resolves(oneAffectedRow);
+      });
+      it("should query the database", async () => {
+        await Budget.update(mockBudgetData);
+        expect(
+          queryDbStub.calledOnceWith("budgets/update.sql", [
+            title,
+            description,
+            id,
+          ])
+        ).to.be.true;
+      });
+      it("should return true", async () => {
+        const result = await Budget.update(mockBudgetData);
+        expect(result).to.be.true;
+      });
+    });
+    describe("If budget does not exist", () => {
+      beforeEach(() => {
+        queryDbStub = sinon.stub(Database, "queryDb").resolves(noAffectedRows);
+      });
+      it("should query the database", () => {
+        return Budget.update(mockBudgetData)
+          .catch(() => {})
+          .finally(() => {
+            expect(
+              queryDbStub.calledOnceWith("budgets/update.sql", [
+                title,
+                description,
+                id,
+              ])
+            ).to.be.true;
+          });
+      });
+      it("should throw an error", () => {
+        return Budget.update(mockBudgetData)
+          .then(() => {
+            throw new Error("update should reject");
+          })
+          .catch((error) => {
+            expect(error.message).to.equal("Budget does not exist");
+          });
+      });
+    });
+    describe("If more than one row is deleted by faulty query", () => {
+      it("should throw an error", () => {
+        sinon.stub(Database, "queryDb").resolves(twoAffectedRows);
+        return Budget.update(mockBudgetData)
+          .then(() => {
+            throw new Error("update() should reject");
+          })
+          .catch((error) => {
+            expect(error.message).to.equal(
+              "Multiple rows updated due to faulty query. Fix budgets/update.sql"
+            );
+          });
+      });
+    });
+    describe("removeById()", () => {
+      describe("If budget exists", () => {
+        beforeEach(() => {
+          queryDbStub = sinon
+            .stub(Database, "queryDb")
+            .resolves(oneAffectedRow);
+        });
+        it("should query the database", async () => {
+          await Budget.removeById(id);
+          expect(queryDbStub.calledOnceWith("budgets/removeById.sql", [id])).to
+            .be.true;
+        });
+        it("should return true", async () => {
+          const result = await Budget.removeById(id);
+          expect(result).to.be.true;
+        });
+      });
+      describe("If budget does not exist", () => {
+        beforeEach(() => {
+          queryDbStub = sinon
+            .stub(Database, "queryDb")
+            .resolves(noAffectedRows);
+        });
+        it("should query the database", () => {
+          return Budget.removeById(id)
+            .catch(() => {})
+            .finally(() => {
+              expect(queryDbStub.calledOnceWith("budgets/removeById.sql", [id]))
+                .to.be.true;
+            });
+        });
+        it("should throw an error", () => {
+          return Budget.removeById(id)
+            .then(() => {
+              throw new Error("removeById should reject");
+            })
+            .catch((error) => {
+              expect(error.message).to.equal("Budget does not exist");
+            });
+        });
+      });
+      describe("If more than one row is deleted by faulty query", () => {
+        it("should throw an error", () => {
+          sinon.stub(Database, "queryDb").resolves(twoAffectedRows);
+          return Budget.removeById(id)
+            .then(() => {
+              throw new Error("removeById should reject");
+            })
+            .catch((error) => {
+              expect(error.message).to.equal(
+                "Multiple rows deleted due to faulty query. Fix budgets/removeById.sql"
+              );
+            });
+        });
       });
     });
   });
