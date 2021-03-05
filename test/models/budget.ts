20b9501c53b4ec94a7143536a372e2d056e0e3ce
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-04 20:17:05 -0600

    Refactor

--- test/models/budget.ts
+++ test/models/budget.ts
@@ -3,6 +3,7 @@ import { OkPacket, RowDataPacket } from "mysql2";
 import sinon, { SinonStub } from "sinon";
 import * as Database from "../../src/database/Database";
 import Budget from "../../src/models/budget";
+import * as Model from "../../src/util/models";
 
 describe("Budget model", () => {
   const newBudgetData = {
@@ -28,49 +29,22 @@ describe("Budget model", () => {
     sinon.restore();
   });
   describe("create()", () => {
-    beforeEach(() => {
-      queryDbStub = sinon.stub(Database, "queryDb").resolves({
-        insertId: 2,
-      } as OkPacket);
-    });
-    it("Should query the database", async () => {
-      await Budget.create(newBudgetData);
-      expect(
-        queryDbStub.calledOnceWith("budgets/create.sql", [title, description])
-      ).to.be.true;
-    });
-    it("Should return the budget id", async () => {
+    it("should call util create() and return its value", async () => {
+      const createStub = sinon.stub(Model, "create").resolves({ _id: id });
       const results = await Budget.create(newBudgetData);
-      expect(results).to.deep.equal({ _id: 2 });
+      expect(createStub.calledOnceWith([title, description], "budget")).to.be
+        .true;
+      expect(results).to.deep.equal({ _id: id });
     });
   });
   describe("findById()", () => {
-    const budgetId = 30;
-    it("should query the database", async () => {
-      queryDbStub = sinon
-        .stub(Database, "queryDb")
-        .resolves([mockBudgetData] as RowDataPacket[]);
-      await Budget.findById(budgetId);
-      expect(queryDbStub.calledOnceWith("budgets/findById.sql", [budgetId])).to
-        .be.true;
-    });
-    describe("If budgetId is in the database...", () => {
-      it("should return complete budget information", async () => {
-        queryDbStub = sinon
-          .stub(Database, "queryDb")
-          .resolves([mockBudgetData] as RowDataPacket[]);
-        const result = await Budget.findById(budgetId);
-        expect(result).to.deep.equal(mockBudgetData);
-      });
-    });
-    describe("If budgetId is not in the database...", () => {
-      it("should return null", async () => {
-        queryDbStub = sinon
-          .stub(Database, "queryDb")
-          .resolves([] as RowDataPacket[]);
-        const result = await Budget.findById(budgetId);
-        expect(result).to.be.null;
-      });
+    it("should call util findById() and return its value", async () => {
+      const findStub = sinon
+        .stub(Model, "findById")
+        .resolves(mockBudgetData as RowDataPacket);
+      const results = await Budget.findById(id);
+      expect(findStub.calledOnceWith(id, "budget")).to.be.true;
+      expect(results).to.deep.equal(mockBudgetData);
     });
   });
   describe("findAllByUserId()", () => {
@@ -104,121 +78,20 @@ describe("Budget model", () => {
     });
   });
   describe("update()", () => {
-    describe("If budget exists", () => {
-      beforeEach(() => {
-        queryDbStub = sinon.stub(Database, "queryDb").resolves(oneAffectedRow);
-      });
-      it("should query the database", async () => {
-        await Budget.update(mockBudgetData);
-        expect(
-          queryDbStub.calledOnceWith("budgets/update.sql", [
-            title,
-            description,
-            id,
-          ])
-        ).to.be.true;
-      });
-      it("should return true", async () => {
-        const result = await Budget.update(mockBudgetData);
-        expect(result).to.be.true;
-      });
+    it("should call util update() and return its value", async () => {
+      const createStub = sinon.stub(Model, "update").resolves(true);
+      const results = await Budget.update(mockBudgetData);
+      expect(createStub.calledOnceWith(id, [title, description], "budget")).to
+        .be.true;
+      expect(results).to.equal(true);
     });
-    describe("If budget does not exist", () => {
-      beforeEach(() => {
-        queryDbStub = sinon.stub(Database, "queryDb").resolves(noAffectedRows);
-      });
-      it("should query the database", () => {
-        return Budget.update(mockBudgetData)
-          .catch(() => {})
-          .finally(() => {
-            expect(
-              queryDbStub.calledOnceWith("budgets/update.sql", [
-                title,
-                description,
-                id,
-              ])
-            ).to.be.true;
-          });
-      });
-      it("should throw an error", () => {
-        return Budget.update(mockBudgetData)
-          .then(() => {
-            throw new Error("update should reject");
-          })
-          .catch((error) => {
-            expect(error.message).to.equal("Budget does not exist");
-          });
-      });
-    });
-    describe("If more than one row is deleted by faulty query", () => {
-      it("should throw an error", () => {
-        sinon.stub(Database, "queryDb").resolves(twoAffectedRows);
-        return Budget.update(mockBudgetData)
-          .then(() => {
-            throw new Error("update() should reject");
-          })
-          .catch((error) => {
-            expect(error.message).to.equal(
-              "Multiple rows updated due to faulty query. Fix budgets/update.sql"
-            );
-          });
-      });
-    });
-    describe("removeById()", () => {
-      describe("If budget exists", () => {
-        beforeEach(() => {
-          queryDbStub = sinon
-            .stub(Database, "queryDb")
-            .resolves(oneAffectedRow);
-        });
-        it("should query the database", async () => {
-          await Budget.removeById(id);
-          expect(queryDbStub.calledOnceWith("budgets/removeById.sql", [id])).to
-            .be.true;
-        });
-        it("should return true", async () => {
-          const result = await Budget.removeById(id);
-          expect(result).to.be.true;
-        });
-      });
-      describe("If budget does not exist", () => {
-        beforeEach(() => {
-          queryDbStub = sinon
-            .stub(Database, "queryDb")
-            .resolves(noAffectedRows);
-        });
-        it("should query the database", () => {
-          return Budget.removeById(id)
-            .catch(() => {})
-            .finally(() => {
-              expect(queryDbStub.calledOnceWith("budgets/removeById.sql", [id]))
-                .to.be.true;
-            });
-        });
-        it("should throw an error", () => {
-          return Budget.removeById(id)
-            .then(() => {
-              throw new Error("removeById should reject");
-            })
-            .catch((error) => {
-              expect(error.message).to.equal("Budget does not exist");
-            });
-        });
-      });
-      describe("If more than one row is deleted by faulty query", () => {
-        it("should throw an error", () => {
-          sinon.stub(Database, "queryDb").resolves(twoAffectedRows);
-          return Budget.removeById(id)
-            .then(() => {
-              throw new Error("removeById should reject");
-            })
-            .catch((error) => {
-              expect(error.message).to.equal(
-                "Multiple rows deleted due to faulty query. Fix budgets/removeById.sql"
-              );
-            });
-        });
-      });
+  });
+  describe("removeById()", () => {
+    it("should call util removeById() and return its value", async () => {
+      const removeStub = sinon.stub(Model, "removeById").resolves(true);
+      const results = await Budget.removeById(id);
+      expect(removeStub.calledOnceWith(id, "budget")).to.be.true;
+      expect(results).to.deep.equal(true);
     });
   });
 });
