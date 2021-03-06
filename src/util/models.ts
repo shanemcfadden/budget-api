c8820f1ce0e3246974b23f0b05cfe2a2b4370450
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-06 14:37:00 -0600

    Add pluralModel()

--- src/util/models.ts
+++ src/util/models.ts
@@ -8,7 +8,7 @@ export interface IdPacket {
 }
 
 export async function findById(id: RowId, model: string) {
-  const results = (await queryDb(`${model}s/findById.sql`, [
+  const results = (await queryDb(`${pluralModel(model)}/findById.sql`, [
     id,
   ])) as RowDataPacket[];
   if (results.length < 1) {
@@ -18,19 +18,22 @@ export async function findById(id: RowId, model: string) {
 }
 
 export async function findAllByBudgetId(id: number, model: string) {
-  return (await queryDb(`${model}s/findAllByBudgetId.sql`, [
+  return (await queryDb(`${pluralModel(model)}/findAllByBudgetId.sql`, [
     id,
   ])) as RowDataPacket[];
 }
 
 export async function findAllByUserId(id: string, model: string) {
-  return (await queryDb(`${model}s/findAllByUserId.sql`, [
+  return (await queryDb(`${pluralModel(model)}/findAllByUserId.sql`, [
     id,
   ])) as RowDataPacket[];
 }
 
 export async function create(data: any[], model: string): Promise<IdPacket> {
-  const results = (await queryDb(`${model}s/create.sql`, data)) as OkPacket;
+  const results = (await queryDb(
+    `${pluralModel(model)}/create.sql`,
+    data
+  )) as OkPacket;
 
   return {
     _id: results.insertId,
@@ -38,7 +41,7 @@ export async function create(data: any[], model: string): Promise<IdPacket> {
 }
 
 export async function update(id: RowId, data: any[], model: string) {
-  const results = (await queryDb(`${model}s/update.sql`, [
+  const results = (await queryDb(`${pluralModel(model)}/update.sql`, [
     ...data,
     id,
   ])) as OkPacket;
@@ -49,18 +52,32 @@ export async function update(id: RowId, data: any[], model: string) {
     throw new Error(`${capitalize(model)} does not exist`);
   }
   throw new Error(
-    `Multiple rows updated due to faulty query. Fix ${model}s/update.sql`
+    `Multiple rows updated due to faulty query. Fix ${pluralModel(
+      model
+    )}/update.sql`
   );
 }
 
 export async function removeById(id: RowId, model: string) {
-  const results = (await queryDb(`${model}s/removeById.sql`, [id])) as OkPacket;
+  const results = (await queryDb(`${pluralModel(model)}/removeById.sql`, [
+    id,
+  ])) as OkPacket;
   if (results.affectedRows === 1) {
     return true;
   } else if (!results.affectedRows) {
     throw new Error(`${capitalize(model)} does not exist`);
   }
   throw new Error(
-    `Multiple rows deleted due to faulty query. Fix ${model}s/removeById.sql`
+    `Multiple rows deleted due to faulty query. Fix ${pluralModel(
+      model
+    )}/removeById.sql`
   );
+}
+
+function pluralModel(modelName: string): string {
+  const oddPlurals: Record<string, string> = {
+    "macro-category": "macro-categories",
+    "micro-category": "micro-categories",
+  };
+  return oddPlurals[modelName] || modelName + "s";
 }
