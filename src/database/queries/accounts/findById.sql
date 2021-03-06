9249a388572950c41209b62e966cc20d9f64f6d5
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-05 19:18:08 -0600

    Add budget_id

--- src/database/queries/accounts/findById.sql
+++ src/database/queries/accounts/findById.sql
@@ -3,6 +3,7 @@ SELECT
     accounts.description AS description,
     start_date AS startDate, 
     start_balance AS startBalance, 
+    budget_id AS budgetId,
     IFNULL(SUM(amount),0) + start_balance AS 'currentBalance' 
 FROM accounts
     JOIN transactions ON transactions.account_id = accounts.id 
