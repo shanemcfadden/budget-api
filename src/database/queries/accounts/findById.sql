2213f523f7721c3f49b4b10169eaa94abd2b86e0
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-05 19:11:06 -0600

    Add currentBalance to account data

--- src/database/queries/accounts/findById.sql
+++ src/database/queries/accounts/findById.sql
@@ -1,3 +1,11 @@
-SELECT * FROM accounts 
-    WHERE id = ? 
+SELECT 
+    name, 
+    accounts.description AS description,
+    start_date AS startDate, 
+    start_balance AS startBalance, 
+    IFNULL(SUM(amount),0) + start_balance AS 'currentBalance' 
+FROM accounts
+    JOIN transactions ON transactions.account_id = accounts.id 
+    WHERE accounts.id = ?
+    GROUP BY accounts.id
 ;
