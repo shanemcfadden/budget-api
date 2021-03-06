9bba0cf80029ebc7a4ff799ff35482b351f84d41
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-06 17:31:33 -0600

    Handle errors

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,43 +7,66 @@ const { JWT_SECRET } = process.env;
 
 export const login: RequestHandler = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await User.findByEmail(email);
-  if (!user) {
-    const error = new Error("Incorrect email or password");
-    next(error);
-    return;
-  }
+  try {
+    const user = await User.findByEmail(email);
+    if (!user) {
+      const error = {
+        statusCode: 404,
+        message: "User not found",
+      };
+      next(error);
+      return;
+    }
 
-  const hashedPassword = user.password;
-  const passwordMatches = await bcrypt.compare(password, hashedPassword);
-  if (!passwordMatches) {
-    const error = new Error("Incorrect email or password");
-    next(error);
-    return;
-  }
+    const hashedPassword = user.password;
+    const passwordMatches = await bcrypt.compare(password, hashedPassword);
+    if (!passwordMatches) {
+      const error = {
+        statusCode: 401,
+        message: "Authentication failed",
+      };
+      next(error);
+      return;
+    }
 
-  const userId = user._id;
-  const token = jwt.sign({ userId }, JWT_SECRET!, { expiresIn: "1h" });
-  res.status(200).json({ message: "Login successful", token });
+    const userId = user._id;
+    const token = jwt.sign({ userId }, JWT_SECRET!, { expiresIn: "1h" });
+    res.status(200).json({ message: "Login successful", token });
+  } catch {
+    next({
+      statusCode: 500,
+      message: "Internal server error",
+    });
+  }
 };
 
 export const signup: RequestHandler = async (req, res, next) => {
   const { email, password, firstName, lastName } = req.body;
-  const user = await User.findByEmail(email);
-  if (user) {
-    const error = new Error("Account for this email already exists");
-    next(error);
-    return;
-  }
+  try {
+    const user = await User.findByEmail(email);
+    if (user) {
+      const error = {
+        statusCode: 401,
+        message: "Account already associated with this email",
+      };
+      next(error);
+      return;
+    }
 
-  const hashedPassword = await bcrypt.hash(password, 12);
-  const newUser = await User.create({
-    email,
-    firstName,
-    lastName,
-    password: hashedPassword,
-  });
-  const userId = newUser._id;
-  const token = jwt.sign({ userId }, JWT_SECRET!, { expiresIn: "1h" });
-  res.status(201).json({ message: "Sign up successful", token });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    });
+    const userId = newUser._id;
+    const token = jwt.sign({ userId }, JWT_SECRET!, { expiresIn: "1h" });
+    res.status(201).json({ message: "Sign up successful", token });
+  } catch {
+    next({
+      statusCode: 500,
+      message: "Internal server error",
+    });
+  }
 };
