8fa401f50a50f87e52605ef40594940f68f63bf8
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-07 16:15:38 -0600

    Add validator Init validation in auth router

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,39 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { login, signup } from "../controllers/auth";
 
 const router = Router();
 
-router.post("/login", login);
+router.post(
+  "/login",
+  body("email").isEmail().normalizeEmail(),
+  body("password").trim().isLength({ min: 8, max: 20 }),
+  login
+);
 
-router.put("/signup", signup);
+router.put(
+  "/signup",
+  body("email").isEmail().isLength({ max: 100 }).normalizeEmail(),
+  body("password")
+    .custom((value, { req }) => {
+      if (value !== req.body.confirmPassword) {
+        throw new Error("Confirm using matching passwords");
+      }
+      return true;
+    })
+    .trim()
+    .custom((value) => {
+      const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+      if (value.match(validPassword)) {
+        return true;
+      }
+      throw new Error(
+        "Password must be 8-20 characters long with at least one of the following: uppercase letter, lowercase letter, number, special character"
+      );
+    }),
+  body("firstName").trim().isLength({ min: 1, max: 100 }),
+  body("lastName").trim().isLength({ min: 1, max: 100 }),
+  signup
+);
 
 export default router;
