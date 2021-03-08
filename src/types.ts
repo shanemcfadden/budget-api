5dee633adb5125dd5ef5d3abea9ecaf79da12198
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-08 15:25:03 -0600

    Add CustomRequestHandler

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,10 @@
-import { Request } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export interface ExtendedRequest extends Request {
   isAuth?: boolean;
   userId?: string;
+}
+
+export interface CustomRequestHandler {
+  (req: ExtendedRequest, res: Response, next: NextFunction): void;
 }
