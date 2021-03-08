876ed7e322270252e8dd9ff9e8ee1ad402df5092
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-08 15:25:24 -0600

    Pass getBudgets() unit tests

--- src/controllers/budget.ts
+++ src/controllers/budget.ts
@@ -1,7 +1,21 @@
 import { RequestHandler } from "express";
+import Budget from "../models/budget";
+import { CustomRequestHandler } from "../types";
+import { handleErrors, ServerError } from "../util/errors";
 
-export const getBudgets: RequestHandler = (req, res, next) => {
-  res.send("GET /budgets");
+export const getBudgets: CustomRequestHandler = async (req, res, next) => {
+  try {
+    if (!req.isAuth) {
+      throw new ServerError(401, "Unauthenticated user");
+    }
+    if (!req.userId) {
+      throw new ServerError(401, "Unauthenticated user");
+    }
+    const results = await Budget.findAllByUserId(req.userId);
+    res.status(200).json(results);
+  } catch (err) {
+    handleErrors(err, next);
+  }
 };
 
 export const getBudget: RequestHandler = (req, res, next) => {
