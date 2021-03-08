f176f331c1e67f6e954a822b67d1a835eb765197
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-08 11:28:41 -0600

    Init test file

---
+++ test/controllers/budget.ts
@@ -0,0 +1,7 @@
+describe("Budget controller", () => {
+  describe("getBudgets()");
+  describe("getBudget()");
+  describe("postBudget()");
+  describe("patchBudget()");
+  describe("deleteBudget()");
+});
