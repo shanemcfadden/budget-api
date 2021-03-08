701c16863de7f196e191c14a155417af34e2eff1
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-08 09:47:52 -0600

    Add validation messages

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,18 +6,29 @@ const router = Router();
 
 router.post(
   "/login",
-  body("email").isEmail().normalizeEmail(),
-  body("password").trim().isLength({ min: 8, max: 20 }),
+  body("email", "Invalid email or password").isEmail().normalizeEmail(),
+  body("password", "Invalid email or password")
+    .trim()
+    .custom((value) => {
+      const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+      if (value.match(validPassword)) {
+        return true;
+      }
+      throw new Error();
+    }),
   login
 );
 
 router.put(
   "/signup",
-  body("email").isEmail().isLength({ max: 100 }).normalizeEmail(),
+  body("email", "Email is invalid")
+    .isEmail()
+    .isLength({ max: 100 })
+    .normalizeEmail(),
   body("password")
     .custom((value, { req }) => {
       if (value !== req.body.confirmPassword) {
-        throw new Error("Confirm using matching passwords");
+        throw new Error("Passwords do not match");
       }
       return true;
     })
@@ -31,8 +42,12 @@ router.put(
         "Password must be 8-20 characters long with at least one of the following: uppercase letter, lowercase letter, number, special character"
       );
     }),
-  body("firstName").trim().isLength({ min: 1, max: 100 }),
-  body("lastName").trim().isLength({ min: 1, max: 100 }),
+  body("firstName", "First name is required")
+    .trim()
+    .isLength({ min: 1, max: 100 }),
+  body("lastName", "Last name is required")
+    .trim()
+    .isLength({ min: 1, max: 100 }),
   signup
 );
 
