e15b346f62ad2d4d59be18e82e78a0e78203550c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-11 10:14:05 -0600

    Incorporate fixtures Add findByIdWithAccountData unit tests

--- test/models/budget.ts
+++ test/models/budget.ts
@@ -1,19 +1,22 @@
 import { expect } from "chai";
 import { RowDataPacket } from "mysql2";
-import sinon from "sinon";
+import sinon, { SinonStub } from "sinon";
 import Budget from "../../src/models/budget";
 import * as Model from "../../src/util/models";
+import * as Database from "../../src/database/Database";
+import {
+  fakeBudgetAccountData,
+  fakeBudgetAccountRows,
+  fakeBudgetData,
+} from "../fixtures";
+import { ServerError } from "../../src/util/errors";
 
 describe("Budget model", () => {
+  const { id, title, description } = fakeBudgetData;
   const newBudgetData = {
-    title: "The Addams Family budget",
-    description: "It's a bit scary...",
+    title,
+    description,
   };
-  const budgetData = {
-    ...newBudgetData,
-    id: 2,
-  };
-  const { id, title, description } = budgetData;
   const budgetDataArr = [title, description];
   const modelName = "budget";
 
@@ -33,27 +36,81 @@ describe("Budget model", () => {
     it("should call util findById() and return its value", async () => {
       const findStub = sinon
         .stub(Model, "findById")
-        .resolves(budgetData as RowDataPacket);
+        .resolves(fakeBudgetData as RowDataPacket);
       const results = await Budget.findById(id);
       expect(findStub.calledOnceWith(id, modelName)).to.be.true;
-      expect(results).to.deep.equal(budgetData);
+      expect(results).to.deep.equal(fakeBudgetData);
     });
   });
+  describe("findByIdWithAccountData()", () => {
+    let queryDbStub: SinonStub;
+    describe("if budget exists...", () => {
+      beforeEach(() => {
+        queryDbStub = sinon
+          .stub(Database, "queryDb")
+          .resolves(fakeBudgetAccountRows);
+      });
+
+      it("should query the database", async () => {
+        await Budget.findByIdWithAccountData(id);
+        expect(queryDbStub.calledOnce).to.be.true;
+        expect(
+          queryDbStub.calledOnceWith("budgets/findByIdWithAccountData.sql")
+        ).to.be.true;
+      });
+      it("should return the budget information with all available accounts", async () => {
+        const results = await Budget.findByIdWithAccountData(id);
+        expect(results).to.deep.equal(fakeBudgetAccountData);
+      });
+    });
+    describe("if budget does not exist", () => {
+      beforeEach(() => {
+        queryDbStub = sinon.stub(Database, "queryDb").resolves([]);
+      });
+      it("should query the database", () => {
+        return Budget.findByIdWithAccountData(id)
+          .then(() => {
+            throw new Error(
+              "Budget.findByIdWithAccountData should reject here"
+            );
+          })
+          .catch(() => {})
+          .finally(() => {
+            expect(queryDbStub.calledOnce).to.be.true;
+            expect(
+              queryDbStub.calledOnceWith("budgets/findByIdWithAccountData.sql")
+            ).to.be.true;
+          });
+      });
+      it("should throw a 404 error", () => {
+        return Budget.findByIdWithAccountData(id)
+          .then(() => {
+            throw new Error(
+              "Budget.findByIdWithAccountData should reject here"
+            );
+          })
+          .catch((err) => {
+            expect(err).to.deep.equal(new ServerError(404, "Budget not found"));
+          });
+      });
+    });
+  });
+  describe("findDetailsById", () => {});
   describe("findAllByUserId()", () => {
     const userId = "asdfwerwqiohon";
     it("should call util findAllByUserId() and return its value", async () => {
       const findStub = sinon
         .stub(Model, "findAllByUserId")
-        .resolves([budgetData as RowDataPacket]);
+        .resolves([fakeBudgetData as RowDataPacket]);
       const results = await Budget.findAllByUserId(userId);
       expect(findStub.calledOnceWith(userId, modelName)).to.be.true;
-      expect(results).to.deep.equal([budgetData]);
+      expect(results).to.deep.equal([fakeBudgetData]);
     });
   });
   describe("update()", () => {
     it("should call util update() and return its value", async () => {
       const createStub = sinon.stub(Model, "update").resolves(true);
-      const results = await Budget.update(budgetData);
+      const results = await Budget.update(fakeBudgetData);
       expect(createStub.calledOnceWith(id, budgetDataArr, modelName)).to.be
         .true;
       expect(results).to.equal(true);
