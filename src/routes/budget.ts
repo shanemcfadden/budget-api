7e8ea8063fc57f8b9e2014de660279161044601c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-11 19:54:14 -0600

    Add validation

--- src/routes/budget.ts
+++ src/routes/budget.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { throwAllValidationErrorMessages } from "../middleware/validator";
 import {
   deleteBudget,
   getBudget,
@@ -9,19 +11,32 @@ import {
 
 const router = Router();
 
-// Get shallow budget details of current user's budgets
 router.get("/", getBudgets);
 
-// Create a new budget
-router.post("/", postBudget);
+router.post(
+  "/",
+  body("title", "Budget title is required").isLength({ min: 1, max: 100 }),
+  body(
+    "description",
+    "Budget description must contain no more than 240 characters"
+  ).isLength({ max: 240 }),
+  throwAllValidationErrorMessages,
+  postBudget
+);
 
-// Get deep data on single budget
 router.get("/:id", getBudget);
 
-// Edit shallow budget details
-router.patch("/:id", patchBudget);
+router.patch(
+  "/:id",
+  body("title", "Budget title is required").isLength({ min: 1, max: 100 }),
+  body(
+    "description",
+    "Budget description must contain no more than 240 characters"
+  ).isLength({ max: 240 }),
+  throwAllValidationErrorMessages,
+  patchBudget
+);
 
-// Delete budget
 router.delete("/:id", deleteBudget);
 
 export default router;
