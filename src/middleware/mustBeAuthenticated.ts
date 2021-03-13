3fa21106c5d49e28273507537a589c12df3027a9
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-13 09:48:43 -0600

    Pass unit tests

---
+++ src/middleware/mustBeAuthenticated.ts
@@ -0,0 +1,16 @@
+import { Response, NextFunction } from "express";
+import { ExtendedRequest } from "../types/express";
+import { ServerError } from "../util/errors";
+
+const mustBeAuthenticated = (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.isAuth || !req.userId) {
+    throw new ServerError(401, "Unauthenticated user");
+  }
+  next();
+};
+
+export default mustBeAuthenticated;
