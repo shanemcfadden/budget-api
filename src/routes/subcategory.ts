4bcb57a49c704b463df03668e4774b2d403f1e26
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-16 10:42:59 -0500

    Add validation

--- src/routes/subcategory.ts
+++ src/routes/subcategory.ts
@@ -1,11 +1,37 @@
 import { RequestHandler, Router } from "express";
+import { body } from "express-validator";
+import { throwAllValidationErrorMessages } from "../middleware/validator";
 import SubcategoryController from "../controllers/subcategory";
 
 const router = Router();
 
-router.post("/", SubcategoryController.postSubcategory as RequestHandler);
+router.post(
+  "/",
+  body("description", "Subcategory description is required").isLength({
+    min: 1,
+  }),
+  body(
+    "description",
+    "Subcategory description must not exceed 100 characters"
+  ).isLength({ max: 100 }),
+  body("categoryId", "Specify a category id").isInt(),
+  throwAllValidationErrorMessages,
+  SubcategoryController.postSubcategory as RequestHandler
+);
 
-router.patch("/:id", SubcategoryController.patchSubcategory as RequestHandler);
+router.patch(
+  "/:id",
+  body("description", "Subcategory description is required").isLength({
+    min: 1,
+  }),
+  body(
+    "description",
+    "Subcategory description must not exceed 100 characters"
+  ).isLength({ max: 100 }),
+  body("categoryId", "Specify a category id").isInt(),
+  throwAllValidationErrorMessages,
+  SubcategoryController.patchSubcategory as RequestHandler
+);
 
 router.delete(
   "/:id",
