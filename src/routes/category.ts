73dc489d0d018d7b322a4f722dfa520b611c2be0
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-16 06:49:38 -0500

    Add validation

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,11 +1,38 @@
 import { RequestHandler, Router } from "express";
+import { body } from "express-validator";
+import { throwAllValidationErrorMessages } from "../middleware/validator";
 import CategoryController from "../controllers/category";
 
 const router = Router();
 
-router.post("/", CategoryController.postCategory as RequestHandler);
+router.post(
+  "/",
+  body("description", "Category description is required").isLength({ min: 1 }),
+  body(
+    "description",
+    "Category description must not exceed 100 characters"
+  ).isLength({ max: 100 }),
+  body(
+    "isIncome",
+    "Specify whether or not this category is for income"
+  ).isBoolean(),
+  body("budgetId", "Specify a budget id").isInt(),
+  CategoryController.postCategory as RequestHandler
+);
 
-router.patch("/:id", CategoryController.patchCategory as RequestHandler);
+router.patch(
+  "/:id", // TODO: Sanitize/validate id param
+  body("description", "Category description is required").isLength({ min: 1 }),
+  body(
+    "description",
+    "Category description must not exceed 100 characters"
+  ).isLength({ max: 100 }),
+  body(
+    "isIncome",
+    "Specify whether or not this category is for income"
+  ).isBoolean(),
+  CategoryController.patchCategory as RequestHandler
+);
 
 router.delete("/:id", CategoryController.deleteCategory as RequestHandler);
 
