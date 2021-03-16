60a3dac8531226df1667418aeb4089e4ce884293
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-16 10:40:09 -0500

    Add deleteSubcategory() unit tests

--- test/controllers/subcategory.ts
+++ test/controllers/subcategory.ts
@@ -289,5 +289,154 @@ describe("SubcategoryController", () => {
       });
     });
   });
-  // describe('deleteSubcategory()')
+  describe("deleteSubcategory()", () => {
+    beforeEach(() => {
+      req = ({
+        userId: fakeUser._id,
+        isAuth: true,
+        params: {
+          id: id.toString(),
+        },
+      } as unknown) as AuthenticatedRequest;
+    });
+    describe("if user is authorized to remove given subcategory...", () => {
+      beforeEach(() => {
+        Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(true);
+      });
+      describe("if subcategory has no transactions...", () => {
+        let subcategoryRemoveStub: SinonStub;
+        beforeEach(() => {
+          Sinon.stub(Subcategory, "hasTransactions").resolves(false);
+        });
+        describe("if remove is successful...", () => {
+          beforeEach(() => {
+            subcategoryRemoveStub = Sinon.stub(
+              Subcategory,
+              "removeById"
+            ).resolves(true);
+          });
+          it("should remove the subcategory", async () => {
+            await deleteSubcategory(req, res as Response, next);
+            expect(subcategoryRemoveStub.calledOnce).to.be.true;
+            expect(subcategoryRemoveStub.calledOnceWith(id)).to.be.true;
+          });
+          it("should send a 200 response", async () => {
+            await deleteSubcategory(req, res as Response, next);
+            expect(res.statusCode).to.equal(200);
+          });
+          it("should send a success message in the response body", async () => {
+            await deleteSubcategory(req, res as Response, next);
+            expect(res.body?.message).to.equal(
+              "Subcategory removed successfully"
+            );
+          });
+        });
+        describe("if the remove is not successful...", () => {
+          beforeEach(() => {
+            subcategoryRemoveStub = Sinon.stub(
+              Subcategory,
+              "removeById"
+            ).rejects(mockInternalServerError);
+          });
+          it("should pass along error rejected by the model", async () => {
+            try {
+              await deleteSubcategory(req, res as Response, next);
+              throw new Error("deleteSubcategory() should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(mockInternalServerError);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await deleteSubcategory(req, res as Response, next);
+              throw new Error("deleteSubcategory() should reject here");
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+      });
+      describe("if subcategory has transactions...", () => {
+        beforeEach(() => {
+          Sinon.stub(Subcategory, "hasTransactions").resolves(true);
+        });
+        it("should pass along a custom 403 error", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch (err) {
+            expect(err).to.deep.equal(
+              new Errors.ServerError(
+                403,
+                "Make sure none of the current transactions are in this subcategory before deleting it"
+              )
+            );
+          }
+        });
+        it("should not send a response", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch {
+          } finally {
+            expect(res.statusCode).to.be.undefined;
+            expect(res.body).to.be.undefined;
+          }
+        });
+      });
+    });
+    describe("if user is not authorized to remove given subcategory...", () => {
+      beforeEach(() => {
+        Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(false);
+      });
+      describe("if subcategory has no transactions...", () => {
+        beforeEach(() => {
+          Sinon.stub(Subcategory, "hasTransactions").resolves(false);
+        });
+        it("should pass along a 403 error", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch (err) {
+            expect(err).to.deep.equal(error403);
+          }
+        });
+        it("should not send a response", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch {
+          } finally {
+            expect(res.statusCode).to.be.undefined;
+            expect(res.body).to.be.undefined;
+          }
+        });
+      });
+      describe("if subcategory has transactions...", () => {
+        beforeEach(() => {
+          Sinon.stub(Subcategory, "hasTransactions").resolves(true);
+        });
+        it("should pass along a 403 error", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch (err) {
+            expect(err).to.deep.equal(error403);
+          }
+        });
+        it("should not send a response", async () => {
+          try {
+            await deleteSubcategory(req, res as Response, next);
+            throw new Error("deleteSubcategory() should reject here");
+          } catch {
+          } finally {
+            expect(res.statusCode).to.be.undefined;
+            expect(res.body).to.be.undefined;
+          }
+        });
+      });
+    });
+  });
 });
