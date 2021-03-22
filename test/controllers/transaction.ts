9e29b0f27be9a282c35baa8df680ed821c698e67
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-22 17:59:41 -0500

    Init unit tests

--- test/controllers/transaction.ts
+++ test/controllers/transaction.ts
@@ -258,6 +258,296 @@ describe("TransactionController", () => {
         },
       } as unknown) as AuthenticatedRequest;
     });
+    describe("if user has permission to update the given transaction...", () => {
+      describe("if user has permission to update given account...", () => {
+        beforeEach(() => {
+          Sinon.stub(User, "hasPermissionToEditAccount").resolves(true);
+        });
+        describe("if user has permission to update given subcategory...", () => {
+          let updateTransactonStub: SinonStub;
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(true);
+          });
+          describe("if transaction update was successful...", () => {
+            beforeEach(() => {
+              updateTransactonStub = Sinon.stub(Transaction, "update").resolves(
+                true
+              );
+            });
+            describe("if retrieving new current balance for the given account is successful...", () => {
+              const mockCurrentBalance = 3000;
+              beforeEach(() => {
+                Sinon.stub(Account, "getCurrentBalance").resolves(
+                  mockCurrentBalance
+                );
+              });
+              it("should update the transaction", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(updateTransactonStub.calledOnce).to.be.true;
+                expect(
+                  updateTransactonStub.calledOnceWith({
+                    id,
+                    amount,
+                    description,
+                    date,
+                    accountId,
+                    subcategoryId,
+                  })
+                ).to.be.true;
+              });
+              it("should send a 200 response", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.statusCode).to.equal(200);
+              });
+              it("should send a success message in the response body", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.body?.message).to.equal(
+                  "Transaction updated successfully"
+                );
+              });
+              it("should send the transaction id in the response body", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.body?.transactionId).to.equal(id);
+              });
+            });
+            describe("if retrieving the new current balance for the given account is not successful...", () => {
+              beforeEach(() => {
+                Sinon.stub(Account, "getCurrentBalance").rejects(
+                  mockInternalServerError
+                );
+              });
+              it("should update the transaction", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(updateTransactonStub.calledOnce).to.be.true;
+                expect(
+                  updateTransactonStub.calledOnceWith({
+                    id,
+                    amount,
+                    description,
+                    date,
+                    accountId,
+                    subcategoryId,
+                  })
+                ).to.be.true;
+              });
+              it("should send a 200 response", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.statusCode).to.equal(200);
+              });
+
+              it("should send a partial success message in the response body", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.body?.message).to.equal(
+                  "Transaction updated successfully"
+                );
+              });
+              it("should send a server error message in the response body", async () => {
+                await patchTransaction(req, res as Response, next);
+                expect(res.body?.error.message).to.equal(
+                  "Internal server error: unable to retrieve current account balance"
+                );
+              });
+            });
+          });
+          describe("if transaction update was not successful...", () => {
+            beforeEach(() => {
+              Sinon.stub(Transaction, "update").rejects(
+                mockInternalServerError
+              );
+            });
+            it("should pass along the error rejected by the model function", async () => {
+              try {
+                await patchTransaction(req, res as Response, next);
+                throw new Error("patchTransaction should reject here");
+              } catch (err) {
+                expect(err).to.deep.equal(mockInternalServerError);
+              }
+            });
+            it("should not send a response", async () => {
+              try {
+                await patchTransaction(req, res as Response, next);
+              } catch {
+              } finally {
+                expect(res.statusCode).to.be.undefined;
+                expect(res.body).to.be.undefined;
+              }
+            });
+          });
+        });
+        describe("if user does not have permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(false);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+      });
+      describe("if user does not have permission to update given account...", () => {
+        beforeEach(() => {
+          Sinon.stub(User, "hasPermissionToEditAccount").resolves(false);
+        });
+        describe("if user has permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(true);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+        describe("if user does not have permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(false);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+      });
+    });
+    describe("if user does not have permission to edit given transaction...", () => {
+      describe("if user has permission to update given account...", () => {
+        beforeEach(() => {
+          Sinon.stub(User, "hasPermissionToEditAccount").resolves(true);
+        });
+        describe("if user has permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(true);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+        describe("if user does not have permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(false);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+      });
+
+      describe("if user does not have permission to update given account...", () => {
+        beforeEach(() => {
+          Sinon.stub(User, "hasPermissionToEditAccount").resolves(false);
+        });
+        describe("if user has permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(true);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+        describe("if user does not have permission to update given subcategory...", () => {
+          beforeEach(() => {
+            Sinon.stub(User, "hasPermissionToEditSubcategory").resolves(false);
+          });
+          it("should throw a 403 error", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+              throw new Error("patchTransaction should reject here");
+            } catch (err) {
+              expect(err).to.deep.equal(error403);
+            }
+          });
+          it("should not send a response", async () => {
+            try {
+              await patchTransaction(req, res as Response, next);
+            } catch {
+            } finally {
+              expect(res.statusCode).to.be.undefined;
+              expect(res.body).to.be.undefined;
+            }
+          });
+        });
+      });
+    });
   });
   describe("deleteTransaction()", () => {
     beforeEach(() => {
