f4dbf7166e34a36b2decb7e51e6eef4761d13241
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-22 17:39:47 -0500

    Pass unit tests

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,3 +1,4 @@
+import Account from "../models/account";
 import Transaction from "../models/transaction";
 import User from "../models/user";
 import { Controller } from "../types/controllers";
@@ -6,7 +7,46 @@ import { handleControllerErrors, ServerError } from "../util/errors";
 
 export const TransactionControllerBase: Controller = {
   postTransaction: (async (req, res, next) => {
-    res.send("POST transaction");
+    const { userId } = req;
+    const { description, amount, date, subcategoryId, accountId } = req.body;
+
+    const [
+      hasSubcategoryPermissions,
+      hasAccountPermissions,
+    ] = await Promise.all([
+      User.hasPermissionToEditSubcategory(userId, subcategoryId),
+      User.hasPermissionToEditAccount(userId, accountId),
+    ]);
+
+    if (!hasAccountPermissions || !hasSubcategoryPermissions)
+      throw new ServerError(403, "Access denied");
+
+    const { _id } = await Transaction.create({
+      description,
+      amount,
+      date,
+      subcategoryId,
+      accountId,
+    });
+
+    let currentBalance: number;
+    try {
+      currentBalance = await Account.getCurrentBalance(accountId);
+    } catch (err) {
+      res.status(200).json({
+        message: "Transaction created successfully",
+        error: {
+          message:
+            "Internal server error: unable to retrieve current account balance",
+        },
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "Transaction created successfully",
+      currentBalance,
+      transactionId: _id,
+    });
   }) as AuthenticatedRequestHandler,
   patchTransaction: (async (req, res, next) => {
     res.send("PATCH transaction");
