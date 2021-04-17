b362ada0a6c2be816bd7f0738d803c90f2133a34
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-17 11:10:35 -0500

    Format transactions queries

--- src/database/queries/transactions/create.sql
+++ src/database/queries/transactions/create.sql
@@ -1,4 +1,10 @@
-INSERT INTO transactions (amount, description, date, account_id, subcategory_id) 
-    VALUES
-        (? , ? , ? , ?, ?)
-;
+INSERT INTO
+    transactions (
+        amount,
+        description,
+        date,
+        account_id,
+        subcategory_id
+    )
+VALUES
+    (?, ?, ?, ?, ?);
--- src/database/queries/transactions/findAllByBudgetId.sql
+++ src/database/queries/transactions/findAllByBudgetId.sql
@@ -1,12 +1,15 @@
-SELECT 
-    transactions.id, 
-    amount, 
-    transactions.description, 
-    date, 
-    account_id AS accountId, 
-    subcategory_id AS subcategoryId 
-FROM transactions
-    JOIN accounts ON transactions.account_id = accounts.id 
-    WHERE accounts.budget_id = ?
-    ORDER BY date DESC, description ASC
-;
+SELECT
+    transactions.id,
+    amount,
+    transactions.description,
+    date,
+    account_id AS accountId,
+    subcategory_id AS subcategoryId
+FROM
+    transactions
+    JOIN accounts ON transactions.account_id = accounts.id
+WHERE
+    accounts.budget_id = ?
+ORDER BY
+    date DESC,
+    description ASC;
