4b8a59373ebcba3d0294ad18d39b50a455882a5e
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-17 11:12:06 -0500

    Format users queries

--- src/database/queries/users/create.sql
+++ src/database/queries/users/create.sql
@@ -1,2 +1,4 @@
-INSERT INTO users (id, email, pw, first_name, last_name)
-    VALUES (?, ?, ?, ?, ?);
+INSERT INTO
+    users (id, email, pw, first_name, last_name)
+VALUES
+    (?, ?, ?, ?, ?);
--- src/database/queries/users/findByEmail.sql
+++ src/database/queries/users/findByEmail.sql
@@ -1,8 +1,10 @@
-SELECT 
-    id AS _id, 
-    email, 
-    pw AS password, 
-    first_name AS firstName, 
-    last_name AS lastName
-  FROM users
-  WHERE email = ?;
+SELECT
+  id AS _id,
+  email,
+  pw AS password,
+  first_name AS firstName,
+  last_name AS lastName
+FROM
+  users
+WHERE
+  email = ?;
