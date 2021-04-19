67135abc9674edb7cacaaee7fa9fa94b247057d6
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-19 14:45:48 -0500

    Quickfix: Add LEFT JOIN

--- src/database/queries/accounts/getCurrentBalance.sql
+++ src/database/queries/accounts/getCurrentBalance.sql
@@ -1,8 +1,8 @@
 SELECT
-    IFNULL(SUM(amount), 0) + a.start_balance AS 'currentBalance'
+    IFNULL(SUM(t.amount), 0) + a.start_balance AS 'currentBalance'
 FROM
     accounts AS a
-    JOIN transactions AS t ON t.account_id = a.id
+    LEFT JOIN transactions AS t ON t.account_id = a.id
 WHERE
     a.id = ?
 GROUP BY
