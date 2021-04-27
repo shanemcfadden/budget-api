60460d55cceaf2312a322eb3def4010ba33f1d60
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-26 20:43:15 -0500

    Refactor account validation

---
+++ src/validation/account.ts
@@ -0,0 +1,51 @@
+import { body, param } from "express-validator";
+import { throwAllValidationErrorMessages } from "middleware/validator";
+
+const validName = body("name", "Account name is required").isLength({
+  min: 1,
+  max: 100,
+});
+
+const validDescription = body(
+  "description",
+  "Account description must not exceed 240 characters"
+).isLength({ max: 240 });
+
+const validStartBalance = body(
+  "startBalance",
+  "Account starting balance is required"
+).isFloat({
+  min: -99999999.99,
+  max: 99999999.99,
+});
+
+const validStartDate = body(
+  "startDate",
+  "Account starting date is required"
+).isDate();
+
+const validBudgetId = body("budgetId", "Budget id is required").isInt();
+
+const validAccountIdParam = param("id", "Invalid account id").isInt();
+
+const AccountValidator = {
+  postAccount: [
+    validName,
+    validDescription,
+    validStartBalance,
+    validStartDate,
+    validBudgetId,
+    throwAllValidationErrorMessages,
+  ],
+  patchAccount: [
+    validAccountIdParam,
+    validName,
+    validDescription,
+    validStartBalance,
+    validStartDate,
+    throwAllValidationErrorMessages,
+  ],
+  deleteAccount: [validAccountIdParam, throwAllValidationErrorMessages],
+};
+
+export default AccountValidator;
