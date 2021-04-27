6c9512e2f656da425103af73e343a04dd9d3ff29
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-26 21:19:13 -0500

    Refactor subcategory validation

---
+++ src/validators/subcategory.ts
@@ -0,0 +1,42 @@
+import { body, param } from "express-validator";
+import { throwAllValidationErrorMessages } from "middleware/validator";
+
+const validateDescriptionIsNotNull = body(
+  "description",
+  "Subcategory description is required"
+).isLength({
+  min: 1,
+});
+const validateDescriptionLength = body(
+  "description",
+  "Subcategory description must not exceed 100 characters"
+).isLength({ max: 100 });
+const validateCategoryId = body("categoryId", "Specify a category id").isInt();
+const validateSubcategoryIdParam = param(
+  "id",
+  "Invalid subcategory id"
+).isInt();
+
+const subcategoryValidatorBase = [
+  validateDescriptionIsNotNull,
+  validateDescriptionLength,
+  validateCategoryId,
+];
+
+const SubcategoryValidator = {
+  postSubcategory: [
+    ...subcategoryValidatorBase,
+    throwAllValidationErrorMessages,
+  ],
+  patchSubcategory: [
+    validateSubcategoryIdParam,
+    ...subcategoryValidatorBase,
+    throwAllValidationErrorMessages,
+  ],
+  deleteSubcategory: [
+    validateSubcategoryIdParam,
+    throwAllValidationErrorMessages,
+  ],
+};
+
+export default SubcategoryValidator;
